6b5f57e1e189a35809f6b9a9bb85dd6de892d8dd
cdq73700 <cdq73700@gmail.com>
2023-01-02 00:52:31 +0900

    console.log削除

--- frontend/app/src/Users/Users.jsx
+++ frontend/app/src/Users/Users.jsx
@@ -4,19 +4,7 @@ import { useUserListQuery } from '../Api'
 import { ItemList } from './ItemList'
 
 export const Users = () => {
-	const users = Array.from({ length: 10 }, (_, index) => {
-		const id = index + 1
-		return {
-			id,
-			email: `test${id}@example.com`,
-			username: `test${id}`,
-			password: `test${id}`,
-		}
-	})
-
 	const userList = useUserListQuery()
-
-	console.log(userList)
 
 	return (
 		<Stack>
