5bb36c769ed2bccd18c294e69bc9e105cdc58b85
cdq73700 <cdq73700@gmail.com>
2023-01-02 16:44:18 +0900

    ルーティング修正

--- frontend/app/src/Router/Router.jsx
+++ frontend/app/src/Router/Router.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
-import { createBrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { App } from '../App'
 import { Users } from '../Users/Users'
 
-export const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />,
-	},
-	{
-		path: 'about',
-		element: <div>About</div>,
-	},
-	{
-		path: 'users',
-		element: <Users />,
-	},
-])
+export const RouterConfig = () => (
+	<>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />} />
+				<Route path="about" element={<div>About</div>} />
+				<Route path="users" element={<Users />}>
+					<Route path=":id" element={<Users />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	</>
+)
